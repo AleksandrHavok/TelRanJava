52299364d9093457be8ea1bb709309191a0a40e1
AleksandrHavok <bogdanov.al.1993@gmail.com>
2020-05-08 23:42:44 +0300

    Repeat lesson 7
    Learn how to use args in psvm and wrapper in variable

---
+++ 2019.07.01-1/src/Repeat/TestArguments.java
@@ -0,0 +1,10 @@
+package Repeat;
+
+public class TestArguments {
+    public static void main(String[] args) {
+        for(int i=0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
+        System.out.println(2+2*2);
+    }
+}
