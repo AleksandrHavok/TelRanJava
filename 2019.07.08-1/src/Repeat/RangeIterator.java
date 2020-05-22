cc956c26db8fe586b4810f38a042e3f77d4d96e5
AleksandrHavok <bogdanov.al.1993@gmail.com>
2020-05-22 18:08:51 +0300

    Repeat lesson 9
    Repeat InnerClasses, intro into Generic Types and Iterator pattern

--- 2019.07.08-1/src/Repeat/RangeIterator.java
+++ 2019.07.08-1/src/Repeat/RangeIterator.java
@@ -9,34 +9,35 @@ public class RangeIterator {
     private int step;
     private boolean reverse;
 
-    public RangeIterator(int start, int end, int step, boolean reverse){
-        this.start=start;
-        this.end=end;
-        this.step=step;
-        this.reverse=reverse;
+    public RangeIterator(int start, int end, int step, boolean reverse) {
+        this.start = start;
+        this.end = end;
+        this.step = step;
+        this.reverse = reverse;
     }
+
     //проверка на возможность сделать еще одну итерацию
-    public boolean hasNext(){
-        return end>=start;
+    public boolean hasNext() {
+        return end >= start;
     }
+
     //возвращает результат итерации исходя из переданных параметров
     //если порядок прямой, то возвращаем стартовое значение
     //затем увеличиваем его его на шаг
     //если порядок обратный, то возвращаем конечное значение
     //затем уменьшаем его на шаг
-    public int next(){
-        if(!hasNext()){
+    public int next() {
+        if (!hasNext()) {
             return -1;
         }
         int res;
-        if(reverse){
-            res=end;
-            end-=step;
+        if (reverse) {
+            res = end;
+            end -= step;
             return res;
-        }
-        else{
-            res=start;
-            start+=step;
+        } else {
+            res = start;
+            start += step;
         }
         return res;
     }
