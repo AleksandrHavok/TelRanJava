45236924103e26968cb688753ed49605f9607fbb
AleksandrHavok <bogdanov.al.1993@gmail.com>
2020-05-22 18:09:57 +0300

    Repeat lesson 9
    Small grammar fix

--- 2019.07.08-2/src/Repeat/TestMyList.java
+++ 2019.07.08-2/src/Repeat/TestMyList.java
@@ -1,15 +1,15 @@
 package Repeat;
 
 public class TestMyList {
-    public static void main(String[] args){
-        MyList <Integer> list = new MyList<>();
+    public static void main(String[] args) {
+        MyList<Integer> list = new MyList<>();
         list.add(10);
         list.add(20);
         list.add(30);
         list.add(40);
         list.add(50);
         list.remove(2);
-        for(int i=0;i<list.size();i++){
+        for (int i = 0; i < list.size(); i++) {
             System.out.println(list.get(i));
         }
 
@@ -17,16 +17,16 @@ public class TestMyList {
         names.add("Sasha");
         names.add("Vasya");
         names.add("Petya");
-        for (int i = 0; i<names.size();i++){
+        for (int i = 0; i < names.size(); i++) {
             System.out.println(names.get(i));
         }
 
         MyList<Person> person = new MyList<>();
-        person.add(new Person("Vasya",22,123));
-        person.add(new Person("Kolya",21,222));
-        person.add(new Person(names.get(0),26,321));
+        person.add(new Person("Vasya", 22, 123));
+        person.add(new Person("Kolya", 21, 222));
+        person.add(new Person(names.get(0), 26, 321));
 
-        for (int i=0;i<person.size();i++){
+        for (int i = 0; i < person.size(); i++) {
             System.out.println(person.get(i).printPerson());
         }
 
