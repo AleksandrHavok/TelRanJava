d4d86627977fd9b885977f3f7de8e8620194db98
AleksandrHavok <bogdanov.al.1993@gmail.com>
2020-06-14 02:27:52 +0300

    Repeat lesson 11
    Intro into class Arrays, Lambdas, Anonimous classes

---
+++ 2019.07.10-2 Interface Comparable/src/PersonNameComparator.java
@@ -0,0 +1,11 @@
+//Помимо имплементации интерфейса, его требуется также импортировать
+import java.util.Comparator;
+
+public class PersonNameComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        //чтобы не писать свой компаратор, можно использовать compareTo
+        //сравнивает 2 строки в лексиграфическом порядке
+        return o1.getName().compareTo(o2.getName());
+    }
+}
---
+++ 2019.07.10-2 Interface Comparable/src/Repeat/Person.java
@@ -0,0 +1,39 @@
+package Repeat;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person (int id,String name,int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String toString(){
+        return id + " " + name + " " + age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return Integer.compare(id, o.id);
+    }
+}
